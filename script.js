a4209e2a8cbbee5324491ad7a4fa2c9282a040ee
binbommmm <dotrunghieu259@gmail.com>
2021-05-07 23:29:29 +0700

    Đã hoàn thành 90% giao diện

---
+++ script.js
@@ -0,0 +1,30 @@
+// Side nav menu
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+let navBar = document.querySelector(".navbar");
+let body = document.querySelector("body");
+
+menuBtn.onclick = function() {
+    menuBtn.style.opacity = "0";
+    menuBtn.style.pointerEvents = "none";
+    navBar.classList.add("active");
+    body.style.overflow = "hidden";
+}
+
+cancelBtn.onclick = function() {
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+    navBar.classList.remove("active");
+    body.style.overflow = "auto";
+}
+
+// Sticky Nav Menu
+let nav = document.querySelector("nav");
+let val;
+window.onscroll = function() {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("sticky");
+    }else{
+        nav.classList.remove("sticky");
+    }
+}
